9c2e6702ba3934b38899ec96692af0c438812cf7
leo Djakam Tientcheu <leo.djakamtientcheu.etu@univ-lille.fr>
2023-09-04 21:25:25 +0200

    modif ennemi

--- src/Ennemi.java
+++ src/Ennemi.java
@@ -1,10 +1,21 @@
-public class Ennemi{
+public enum Ennemi{
+    ZOMBIE(50,30,2,5), RAT(20,10,3,3), COCKROACH(5,2,3,1);
+
     private int health;
     private int damage;
-    
-    public Ennemi(int health, int damage){
+    private int range;
+    private int cost;
+    //ajouter drop, taux de drop
+
+    private Ennemi(int health, int damage, int range, int cost){
         this.health = health;
         this.damage = damage;
+        this.range = range;
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
     }
 
     public int getDamage() {
@@ -15,6 +26,14 @@ public class Ennemi{
         return health;
     }
 
+    public int getRange() {
+        return range;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
     public void setDamage(int damage) {
         this.damage = damage;
     }
@@ -23,4 +42,10 @@ public class Ennemi{
         this.health = health;
     }
 
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    
 }
+
