66e9603888a2f6c5baae5a161d9c269560b5b848
Pierre Planchon <pierre.planchon.etu@univ-lille.fr>
2023-09-05 16:35:38 +0200

    test de player

---
+++ test/PlayerTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import entity.Item;
+import entity.Player;
+
+
+public class PlayerTest {
+    
+    Player player = new Player(10, 10, "Player 1");
+    Item cle = Item.KEY;
+    ArrayList<Item> compare;
+    Item cle2 = Item.KEY;
+
+    @Test
+    public void statsToStringTest(){
+        assertEquals("Player 1; hp:100/100, state: "+player.getState().toString()+", ammo: 10", player.statsToString());
+    }
+    
+    @Test
+    public void addInventoryTest(){
+        compare.add(cle2);
+        player.addInventory(cle);
+        assertEquals(player.getItems(), compare);
+    }
+
+    @Test
+    public void supInventoryTest(){
+        assertTrue(player.supInventory(cle));
+        assertFalse(player.supInventory(Item.HEAL));
+    }
+
+    @Test
+    public void countItemTest(){
+        player.addInventory(Item.GRENADE);
+        player.addInventory(Item.GRENADE);
+        assertEquals(2, Item.GRENADE);
+    }
+
+    @Test
+    public void consommerItemTest(){
+        player.setHealth(50);
+        player.addInventory(Item.HEAL);
+        player.consommerItem(Item.HEAL);
+        assertEquals(60, player.getHealth());
+
+        player.setHealth(100);
+        player.addInventory(Item.HEAL);
+        player.consommerItem(Item.HEAL);
+        assertEquals(100, player.getHealth());
+
+        player.setHealth(95);
+        player.addInventory(Item.HEAL);
+        player.consommerItem(Item.HEAL);
+        assertEquals(100, player.getHealth());
+
+        player.addInventory(Item.AMO);
+        player.consommerItem(Item.AMO);
+        assertEquals(15, player.getAmmo());
+    }
+}
